1f4b0a1659fece8831e8ae22ffc3f61cef2500a1
Ashish Patel <ashishp16.2000@gmail.com>
2025-11-17 04:19:22 +0100

    sync main

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
     print("Hello from kg-guided-codegen!")
 
-
+from benchmarks.dataset_loader import load_humaneval
+from program_analysis.file_utils import dump_to_json
 if __name__ == "__main__":
-    main()
+    data = load_humaneval()
+    dataset = []
+    for i in data:
+        dataset.append(i)
+    dump_to_json("humaneval.json", dataset)
+    print(data[0], type(data[0]))
+
